f1f34d675b6c81c6ad93fea7327dbc4f7a6e21b0
patty1xd <alexandruflorea2019@gmail.com>
2026-04-26 13:32:32 +0300

    Create StatsHologramsPlugin.java

---
+++ src/main/java/com/statshologram/StatsHologramsPlugin.java
@@ -0,0 +1,67 @@
+package com.statshologram;
+
+import com.statshologram.commands.HologramCommand;
+import com.statshologram.data.PlayerStatsManager;
+import com.statshologram.holograms.HologramManager;
+import com.statshologram.listeners.PlayerStatsListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StatsHologramsPlugin extends JavaPlugin {
+    
+    private static StatsHologramsPlugin instance;
+    private PlayerStatsManager statsManager;
+    private HologramManager hologramManager;
+    
+    @Override
+    public void onEnable() {
+        instance = this;
+        
+        // Save default configuration
+        saveDefaultConfig();
+        
+        // Initialize managers
+        statsManager = new PlayerStatsManager(this);
+        hologramManager = new HologramManager(this);
+        
+        // Register commands
+        getCommand("hologram").setExecutor(new HologramCommand(this));
+        
+        // Register listeners
+        getServer().getPluginManager().registerEvents(new PlayerStatsListener(this), this);
+        
+        // Start hologram update task
+        int updateInterval = getConfig().getInt("update-interval", 100);
+        getServer().getScheduler().runTaskTimer(this, () -> {
+            hologramManager.updateAllHolograms();
+        }, 20L, updateInterval);
+        
+        getLogger().info("StatsHolograms has been enabled!");
+    }
+    
+    @Override
+    public void onDisable() {
+        // Remove all holograms
+        if (hologramManager != null) {
+            hologramManager.removeAllHolograms();
+        }
+        
+        // Save stats data
+        if (statsManager != null) {
+            statsManager.saveData();
+        }
+        
+        getLogger().info("StatsHolograms has been disabled!");
+    }
+    
+    public static StatsHologramsPlugin getInstance() {
+        return instance;
+    }
+    
+    public PlayerStatsManager getStatsManager() {
+        return statsManager;
+    }
+    
+    public HologramManager getHologramManager() {
+        return hologramManager;
+    }
+}
